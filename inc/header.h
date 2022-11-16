8bf6a06f101982d622920f4b4187cbfe29290e17
Erik-Asatryan <105886121+Erik-Asatryan@users.noreply.github.com>
2022-11-16 19:15:24 +0400

    Update header.h

--- inc/header.h
+++ inc/header.h
@@ -4,6 +4,9 @@ typedef std::string str;
 #ifndef __CODER__
 #define __CODER__
 
+# Բոլոր ֆունկցիաները մի header֊ում հայտարարելը այդքան էլ լավ գաղափար չէ։
+# Ամեն մի .h ֆայլին պետք է համապատասխանի մեկ .cpp ֆայլ։
+# Ինչպես նաև ցանկալի է, որ ամեն գրադարանի համապատասխան ֆայլերը inc, src, obj֊ներում առանձնացված լինեն գրադարանի անունով directory֊ներում
 str encoder(str);
 str decoder(str);
 void unit_test(str);
